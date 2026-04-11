7d808585d6f3f96a324f71e7d29de8c7b265158a
Julien-Dussaut <julien.dussaut.oclock@gmail.com>
2026-04-11 13:14:47 +0200

    Architecture done. Html for landing page done.

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+import { 
+  createIcons,
+  CircleQuestionMark,
+  Check,
+  ListFilter,
+  LayoutGrid,
+  CircleCheck,
+  ArrowUp,
+  ChartNoAxesCombined,
+  WandSparkles,
+  Clock,
+  CircleAlert } from "lucide";
+
+createIcons({
+  icons: {
+    CircleQuestionMark,
+    Check,
+    ListFilter,
+    LayoutGrid,
+    CircleCheck,
+    ArrowUp,
+    ChartNoAxesCombined,
+    WandSparkles,
+    Clock,
+    CircleAlert
+  }
+});
